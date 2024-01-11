916266ed7e8cf4175ea6ec7ec9e5aa720f37bd88
yeonhoryu <yhryu.irs@gmail.com>
2024-01-10 21:15:50 -0500

    initialized commit

---
+++ posts.js
@@ -0,0 +1,12 @@
+var posts = [{
+  title: "MIT",
+  content:"Great place to hang out"
+},
+{
+  title: "cricket",
+  content:"Take a long time to master"
+},
+{
+  title: "Good Article",
+  content:"Death of planning"
+}];
